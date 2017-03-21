da321ed2887e86941bcfa759f897cc777bdd14c3
kebharta <ic16b023@technikum-wien.at>
2017-03-21 16:24:03 +0100

    add project Task4

---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+
+{
+
+    public interface BaseData
+    {
+        string Title
+        {
+            get;
+        }
+
+        string Media
+        {
+            get;
+        }
+
+        int Tonality
+        {
+            get;
+        }
+
+    }
+
+    public class Article : BaseData
+    {
+        private double _sitePrice;
+
+        public double SitePrice
+        {
+            get
+            {
+                return _sitePrice;
+            }
+
+            set
+            {
+                if (value < 0) throw new Exception("Site price must not be negative.");
+                _sitePrice = value;
+            }
+        }
+
+        public Article(string title, string media, double size, double sitePrice, int tonality)
+        {
+            if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title must not be empty.", nameof(title));
+            if (string.IsNullOrWhiteSpace(media)) throw new ArgumentException("Media must not be empty.", nameof(media));
+            if (size < 0) throw new ArgumentOutOfRangeException("Size must not be negative");
+            if (tonality < -1 || tonality > 1) throw new ArgumentOutOfRangeException("Tonality must be positive (1), negative (-1) or neutral (0)");
+
+            Title = title;
+            Media = media;
+            Size = size;
+            SitePrice = sitePrice;
+            Tonality = tonality;
+            CommercialValue = CalcCommercialValue(sitePrice, size);
+
+        }
+
+        public string Title
+        {
+            get;
+        }
+
+        public string Media
+        {
+            get;
+        }
+
+        public double Size
+        {
+            get;
+        }
+
+        public int Tonality
+        {
+            get;
+        }
+
+        public double CommercialValue
+        {
+            get;
+        }
+
+        public double CalcCommercialValue(double SitePrice, double size)
+        {
+            return SitePrice * size;
+        }
+
+
+    }
+
+    public class Posting : BaseData
+    {
+
+        public Posting(string title, string media, string username, int tonality)
+        {
+            if (string.IsNullOrWhiteSpace(title)) throw new ArgumentException("Title must not be empty.", nameof(title));
+            if (string.IsNullOrWhiteSpace(media)) throw new ArgumentException("Media must not be empty.", nameof(media));
+            if (tonality < -1 || tonality > 1) throw new ArgumentOutOfRangeException("Tonality must be positive (1), negative (-1) or neutral (0)");
+            if (string.IsNullOrWhiteSpace(username)) throw new ArgumentException("Username must not be empty.", nameof(username));
+
+
+            Title = title;
+            Media = media;
+            Username = username;
+            Tonality = tonality;
+        }
+
+        public string Title
+        {
+            get;
+        }
+
+        public string Media
+        {
+            get;
+        }
+
+        public int Tonality
+        {
+            get;
+        }
+
+        public string Username
+        {
+            get;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var mentioned = new BaseData[]
+                {
+                    new Article("Leberkas Pepi eröffnet am Wiener Hauptbahnhof", "Heute", 0.25, 23827, 1),
+                    new Posting("Endlich guter Leberkäse in Wien #Leberkas Pepi", "twitter.com", "LeberkasLover", 1),
+                    new Posting("Hier stinkt's!!1! Danke #Leberkas Pepi :-(", "twitter.com", "nofun", -1)
+
+                };
+
+                foreach (var y in mentioned)
+                {
+                    Console.WriteLine($"{y.Media}: {y.Title} mit Tonalität {y.Tonality}");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Fehler! " + e.Message);
+                return;
+            }
+        }
+    }
+}
---
+++ tasks/Task4/Task4/Test.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task4
+{
+    [TestFixture]
+
+    class Test
+    {
+        [Test]
+        public void CanCreateArticle()
+        {
+            var x = new Article("Leberkas Pepi eröffnet am Wiener Hauptbahnhof", "Heute", 0.25, 23827, 1);
+
+            Assert.IsTrue(x.Title == "Leberkas Pepi eröffnet am Wiener Hauptbahnhof");
+            Assert.IsTrue(x.Media == "Heute");
+            Assert.IsTrue(x.Size == 0.25);
+            Assert.IsTrue(x.SitePrice == 23827);
+            Assert.IsTrue(x.Tonality == 1);
+        }
+
+        [Test]
+        public void CannotCreateArticleWithEmptyTitle1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Article(null, "Heute", 0.25, 23827, 1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateArticleWithEmptyTitle2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Article("", "Heute", 0.25, 23827, 1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateArticleWithEmptyMedia1()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Article("Leberkas Pepi eröffnet am Wiener Hauptbahnhof", null, 0.25, 23827, 1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateArticleWithEmptyMedia2()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Article("Leberkas Pepi eröffnet am Wiener Hauptbahnhof", "", 0.25, 23827, 1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateArticleWithNegativeSize()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Article("Leberkas Pepi eröffnet am Wiener Hauptbahnhof", "Heute", -1, 23827, 1);
+            });
+        }
+
+        [Test]
+        public void CannotCreateArticleWithNegativeSitePrice()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Article("Leberkas Pepi eröffnet am Wiener Hauptbahnhof", "Heute", 0.25, -1, 1);
+            });
+        }
+
+        [Test]
+        public void CanCalculateCommercialValueWithPrice()
+        {
+            var x = new Article("Leberkas Pepi eröffnet am Wiener Hauptbahnhof", "Heute", 0.25, 23827, 1);
+            x.CalcCommercialValue(23827, 0.25);
+
+            Assert.IsTrue(x.SitePrice == 23827);
+            Assert.IsTrue(x.Size == 0.25);
+        }
+    }
+}
